0270738399569118da2d3fb78e471db95679c8e0
Vidit Jha <jhavidit@gmail.com>
2022-02-23 13:14:59 +0530

    Middle of Linked List

---
+++ linkedList/MiddleOfLinkedList.java
@@ -0,0 +1,14 @@
+package linkedList;
+
+public class MiddleOfLinkedList {
+
+    public ListNode middle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
