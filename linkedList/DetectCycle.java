da8ccc5f7b8a0fd853288cd555e15691e36d0acd
Vidit Jha <jhavidit@gmail.com>
2022-03-01 10:40:08 +0530

    DetectCycle

---
+++ linkedList/DetectCycle.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+public class DetectCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null)
+            return false;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+
+}
