42f9d14793ca6908cdc927c6bb664af2aa694145
Vidit Jha <jhavidit@gmail.com>
2022-07-03 14:12:32 +0530

    Valid Parenthesis

---
+++ stackAndQueue/validParanthesis.java
@@ -0,0 +1,35 @@
+package stackAndQueue;
+
+import java.util.Stack;
+
+public class validParanthesis {
+
+    public static boolean isValid(String s) {
+        int l = s.length();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < l; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else if (ch == ')' && stack.peek() != '(') {
+                    return false;
+                } else if (ch == '}' && stack.peek() != '{') {
+                    return false;
+                } else if (ch == ']' && stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}"));
+    }
+
+}
