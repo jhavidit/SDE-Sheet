a9b3f44acf37e5f89522e121c92b9ab4c2c06fe6
Vidit Jha <jhavidit@gmail.com>
2022-07-05 19:34:43 +0530

    MaximumProductSubArray

--- dynamicProgramming/day1/MaximumProductSubArray.java
+++ dynamicProgramming/day1/MaximumProductSubArray.java
@@ -7,7 +7,7 @@ public class MaximumProductSubArray {
         int n = nums.length;
         for (int i = 1; i < n; i++) {
             int temp = Math.max(nums[i], Math.max(prod1 * nums[i], prod2 * nums[i]));
-            prod2 = Math.min(nums[i], Math.max(prod1 * nums[i], prod2 * nums[i]));
+            prod2 = Math.min(nums[i], Math.min(prod1 * nums[i], prod2 * nums[i]));
             prod1 = temp;
             res = Math.max(res, prod1);
         }
